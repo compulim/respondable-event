f53c0ceba9b8fd85bfef1c41177a843e4fca3b64
William Wong <compulim@users.noreply.github.com>
2024-12-10 03:40:40 +0000

    Add describe

--- packages/integration-test/test/webDriver/sample/test.mjs
+++ packages/integration-test/test/webDriver/sample/test.mjs
@@ -5,21 +5,25 @@ import { Browser, Builder } from 'selenium-webdriver';
 /** @type {import("selenium-webdriver").WebDriver} */
 let driver;
 
-beforeEach(async () => {
-  driver = await new Builder().forBrowser(Browser.CHROME).usingServer('http://localhost:4444/wd/hub').build();
-});
+describe('User story on window.addEventListener', () => {
+  beforeEach(async () => {
+    driver = await new Builder().forBrowser(Browser.CHROME).usingServer('http://localhost:4444/wd/hub').build();
+  });
 
-afterEach(() => driver?.quit());
+  afterEach(() => driver?.quit());
 
-it('should work', async () => {
-  await driver.get('http://web/test/sample/');
+  it('should work', async () => {
+    await driver.get('http://web/test/sample/');
 
-  // @ts-expect-error
-  await driver.executeAsyncScript(done =>
-    document.readyState === 'loading' ? window.addEventListener('DOMContentLoaded', done) : done()
-  );
+    // @ts-expect-error
+    await driver.executeAsyncScript(done =>
+      document.readyState === 'loading' ? window.addEventListener('DOMContentLoaded', done) : done()
+    );
 
-  await driver.wait(() => driver.executeScript(() => !!document.querySelector('h1')?.textContent));
+    await driver.wait(() => driver.executeScript(() => !!document.querySelector('h1')?.textContent));
 
-  await expect(driver.executeScript(() => document.querySelector('h1')?.textContent)).resolves.toEqual('Hello, World!');
+    await expect(driver.executeScript(() => document.querySelector('h1')?.textContent)).resolves.toEqual(
+      'Hello, World!'
+    );
+  });
 });
